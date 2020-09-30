074a2eedf5111e9996f70407171e781c64c34207
Adaware <nopsye@gmail.com>
2020-09-30 20:25:06 +0200

    Aggiungo Boostrap e Modifico Il Form

--- webserver.js
+++ webserver.js
@@ -1,10 +1,14 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4000;
+
+
+app.use('/static', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/html/chat.html');
 });
 
 io.on('connection', function(socket){
