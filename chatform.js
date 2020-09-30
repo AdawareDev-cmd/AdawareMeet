18198b7c6004ad4ccd4d5ead737cd1e4278ad883
Adaware <nopsye@gmail.com>
2020-09-30 19:46:26 +0200

    Aggiunti I Vecchi File

---
+++ chatform.js
@@ -0,0 +1,12 @@
+$(function () {
+    var socket = io();
+    $('#chatform').submit(function(){
+      socket.emit('chat message', $('#m').val());
+      $('#m').val('');
+      return false;
+    });
+    socket.on('chat message', function(msg){
+      $('#messages').append($('<li>').text(msg));
+      window.scrollTo(0, document.body.scrollHeight);
+    });
+  });
